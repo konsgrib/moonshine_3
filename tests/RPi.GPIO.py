367bdf16282b3e8a3fe1b35c82f1bb52b06e03e5
konsgrib <konstantin@gribanov.net>
2024-05-28 20:06:34 +0300

    tests6

---
+++ tests/RPi.GPIO.py
@@ -0,0 +1,34 @@
+# gpio_stub.py
+class GPIO:
+    BOARD = None
+    OUT = None
+    IN = None
+    HIGH = None
+    LOW = None
+    PUD_UP = None
+    PUD_DOWN = None
+
+    @staticmethod
+    def setmode(mode):
+        pass
+
+    @staticmethod
+    def setup(channel, state):
+        pass
+
+    @staticmethod
+    def output(channel, state):
+        pass
+
+    @staticmethod
+    def input(channel):
+        return 0
+
+    @staticmethod
+    def cleanup():
+        pass
+
+
+
+
+
